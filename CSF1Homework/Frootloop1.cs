f9a559f300d14b7114b619365baac23342c7a549
Logan Rothrock <loganrothrock@outlook.com>
2020-08-28 15:42:15 -0500

    FruitLoops Done

---
+++ CSF1Homework/Frootloop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FrootLoop1
+    {
+        static void Main(string[] args)
+        {
+            string[] fruit = { "Cherry","Apple","Banana","Mango","Avacado","Watermelon"};
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.Write($"{fruit[i]}\n");
+            }
+
+        }
+    }
+}
---
+++ CSF1Homework/FruitLoop2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop2
+    {
+        static void Main(string[] args)
+        {
+            string[] fruit = { "Cherry", "Apple", "Banana", "Mango", "Avacado", "Watermelon" };
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                byte period = 0;
+                Console.Write($"{fruit[i]}\n");
+                while (period <= i)
+                {
+                    Console.Write(".");
+                    period++;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
