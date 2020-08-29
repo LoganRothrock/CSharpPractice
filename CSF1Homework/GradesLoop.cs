3ed80be52d208f36b74cf7c7083bd17f5561cf8e
Logan Rothrock <loganrothrock@outlook.com>
2020-08-29 14:22:06 -0500

    GradesLoop

---
+++ CSF1Homework/GradesLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class GradesLoop
+    {
+        static void Main(string[] args)
+        {
+            sbyte score = 0;
+            do
+            {
+                Console.Write("Please enter a score: ");
+                score = sbyte.Parse(Console.ReadLine());
+                
+                if(score >= 0)
+                {
+                    Console.WriteLine(score);
+                }
+                    
+            } while (score >=0);
+        }
+    }
+}
---
+++ CSF1Homework/Powers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=50; i++)
+            {
+                long sum = 0;
+                sum = Convert.ToInt64(Math.Pow(2,i));
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
