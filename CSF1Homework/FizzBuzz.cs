26140b487720a528da519591ebae1b79f46fe74e
Logan Rothrock <loganrothrock@outlook.com>
2020-08-30 20:52:36 -0500

    FizzBuzz completed

---
+++ CSF1Homework/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number for a FizzBuzz game: ");
+            int userNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= userNum; i++)
+            {
+                if (i % 5 ==0 && i % 3==0)
+                {
+                    Console.Write("FizzBuzz, ");
+                }
+                else if (i % 3 ==0)
+                {
+                    Console.Write("Fizz, ");
+                }
+                else if (i % 5 ==0)
+                {
+                    Console.Write("Buzz, ");
+                }
+                else
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
